793988fbedd71bea02787e5aebc650ceaf7a8d19
郭新源 <1329935210@qq.com>
2022-04-14 16:34:05 +0800

    GreeSpirit提交

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
+  title: '瀚高数据库',
   tagline: 'Dinosaurs are cool',
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
@@ -22,91 +22,142 @@ const config = {
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
+          routeBasePath: '/',
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          // editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
+        // blog: {
+        //   showReadingTime: true,
+        //   // Please change this to your repo.
+        //   editUrl:
+        //     'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        // },
+        blog: false,
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
       }),
     ],
   ],
-
+  plugins: [
+    [
+      '@docusaurus/plugin-content-docs',
+      {
+        id: 'community',
+        versions: {
+          current: {label: '运维指南'}
+        },
+        path: 'community',
+        routeBasePath: 'community',
+        sidebarPath: require.resolve('./sidebars.js'),
+        // ... 其他设置
+      },
+    ],
+    [
+      '@docusaurus/plugin-content-docs',
+      {
+        id: 'applicationDep',
+        versions: {
+          current: {label: '应用开发指南'}
+        },
+        path: 'applicationDep',
+        routeBasePath: 'applicationDep',
+        sidebarPath: require.resolve('./sidebars.js'),
+        // ... 其他设置
+      },
+    ],
+    [
+      '@docusaurus/plugin-content-docs',
+      {
+        id: 'sql',
+        versions: {
+          current: {label: 'SQL开发指南'}
+        },
+        path: 'sql',
+        routeBasePath: 'sql',
+        sidebarPath: require.resolve('./sidebars.js'),
+        // ... 其他设置
+      },
+    ],
+    [
+      '@docusaurus/plugin-content-docs',
+      {
+        id: 'problemMenu',
+        versions: {
+          current: {label: '常见问题'}
+        },
+        path: 'problemMenu',
+        routeBasePath: 'problemMenu',
+        sidebarPath: require.resolve('./sidebars.js'),
+        // ... 其他设置
+      },
+    ],
+    [
+      '@docusaurus/plugin-content-docs',
+      {
+        id: 'manualDep',
+        versions: {
+          current: {label: '产品手册'}
+        },
+        path: 'manualDep',
+        routeBasePath: 'manualDep',
+        sidebarPath: require.resolve('./sidebars.js'),
+        // ... 其他设置
+      },
+    ],
+  ],
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'My Site',
         logo: {
           alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          src: 'img/logo-doc.svg',
         },
         items: [
           {
             type: 'doc',
             docId: 'intro',
             position: 'left',
-            label: 'Tutorial',
+            label: '快速入手',
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/facebook/docusaurus',
-            label: 'GitHub',
-            position: 'right',
+            type: 'docsVersionDropdown',
+            docsPluginId: 'community',
+            activeBasePath: '/community/'
           },
+          {
+            type: 'docsVersionDropdown',
+            docsPluginId: 'applicationDep',
+            activeBasePath: '/applicationDep/'
+          },
+          {
+            type: 'docsVersionDropdown',
+            docsPluginId: 'sql',
+            activeBasePath: '/sql/'
+          },
+          {
+            type: 'docsVersionDropdown',
+            docsPluginId: 'problemMenu',
+            activeBasePath: '/problemMenu/'
+          },
+          {
+            type: 'docsVersionDropdown',
+            docsPluginId: 'manualDep',
+            activeBasePath: '/problemMenu/'
+          },
+          // {
+          //   href: 'https://github.com/facebook/docusaurus',
+          //   label: 'GitHub',
+          //   position: 'right',
+          // },
         ],
       },
       footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        // style: 'dark',
+        copyright: `Copyright © 2021 瀚高基础软件股份有限公司 | 备案号：鲁ICP备09010331号-3`,
+       
       },
       prism: {
         theme: lightCodeTheme,
